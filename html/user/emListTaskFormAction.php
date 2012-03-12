0d3dd3668e3dc23074c4ee509a2461a0eb6462a4
rampxxxx <rampxxxx@gmail.com>
2012-03-12 03:32:01 +0100

    Lista informativa de ficheros/bbdd borrado.

--- html/user/emListTaskFormAction.php
+++ html/user/emListTaskFormAction.php
@@ -14,6 +14,8 @@ $user = get_logged_in_user();
 
 echo "Tamano parametrosss  :(" . $size . ")";
 
+page_head(tra("List Of Deleted Files "));
+start_table("align=\"center\"");
 $contador=1;
 foreach($_REQUEST as $key => $value)
 {
@@ -22,12 +24,18 @@ echo "key : " . $key . " value : " . $value . "<br/>";
 	$fichero0=$pathBorrado . $key . "_0.gz";
 	$fichero1=$pathBorrado . $key . "_1.gz";
 	$salida=shell_exec('rm -f ' . $fichero0 . " " . $fichero1);
-$result=mysql_query("delete from user_workunit where workunit_id = " . $value );
+
+        echo "<tr><td> Deleted File :" . $fichero0 . "</td>";
+        echo "<tr><td> Deleted File :" . $fichero1 . "</td>";
+	$result=mysql_query("delete from user_workunit where workunit_id = " . $value );
+        echo "<tr><td> Deleted Database References </td>";
 	$contador+=1;	
 } 
 echo "FICHEROS BORRADOS!!!";
 
 
+end_table();
+page_tail();
 
 
 ?>
