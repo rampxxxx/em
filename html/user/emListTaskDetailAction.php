914c7f072713f8b0851b8507d0b58188e096fc36
rampxxxx <rampxxxx@gmail.com>
2012-03-12 05:57:17 +0100

    Lista de parametros.

---
+++ html/user/emListTaskDetailAction.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+
+<?php
+require_once("../inc/db.inc");
+require_once("../inc/util.inc");
+require_once("../inc/countries.inc");
+
+$size = (int) count($_REQUEST);
+
+
+db_init();
+$user = get_logged_in_user();
+
+
+page_head(tra("Parameters "));
+start_table("align=\"center\"");
+$contador=1;
+foreach($_REQUEST as $key => $value)
+{
+
+	$result=mysql_query("select parameters from user_workunit where workunit_id = " . $value );
+	while($row=mysql_fetch_array($result))
+	{
+		echo "<tr><td> " . $row['parameters'] . "</td>";
+	}
+
+	$contador+=1;	
+} 
+
+
+end_table();
+page_tail();
+
+
+?>
+
+</body>
+</html>
