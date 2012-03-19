04c9cbb4a1640c6e0b869b3ad940026b2179ed06
rampxxxx <rampxxxx@gmail.com>
2012-03-19 03:38:14 +0100

    Script lanza.sh.

---
+++ bin/lanza.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PROJECT_ROOT=../..
+
+
+cd $PROJECT_ROOT # go
+
+pwd >> /tmp/lanza.txt
+echo "Desde el script lanza.sh" >> /tmp/lanza.txt
+bin/sample_work_generator_em -d 3 --app em --in_template_file em_in.xml --out_template_file em_out.xml --user_id $1 --alias $2 >> /tmp/lanza.txt 2>&1
+
+cd - #back
