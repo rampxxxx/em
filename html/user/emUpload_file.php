f9ef7d1f17ff9034d6eb82c8c673120a7840a682
rampxxxx <rampxxxx@gmail.com>
2012-04-29 20:23:57 +0200

    GitHub Issues : closes  #16  : gestion de diferentes versiones con diferentes plataformas.

--- html/user/emUpload_file.php
+++ html/user/emUpload_file.php
@@ -24,21 +24,34 @@ $dataDir="../../backupEm/";
 $programName="emProgramName";
       move_uploaded_file($_FILES["file"]["tmp_name"],
       $dataDir . $programName);
-      echo "Stored in: " . $dataDir . $programName;
+      echo "Stored in: " . $dataDir . $programName . "<br>";
       $versiones=get_mysql_lastVersion("select max(version_num) from app_version,platform where platform.id=app_version.platformid");
       foreach($versiones as $version)
       {
 	      $lastVersion=$version["max(version_num)"];
       }
 echo "lastVersion : " . $lastVersion . "<br>";
-      $cien=100;
-      $mayor=floor($lastVersion/$cien);
-      $minor=$lastVersion%$cien;
+$queryMyVersion="select max(version_num) from app_version,platform where app_version.platformid=platform.id and platform.name='". $_REQUEST["platform"] . "'";
+      $myVersions=get_mysql_myVersion($queryMyVersion);
+      foreach($myVersions as $myVersion)
+      {
+	      $myversion=$myVersion["max(version_num)"];
+      }
+echo "myversion : " . $myversion . "<br>";
+echo "platform : " . $_REQUEST["platform"] . "<br>";
+echo "query  : " . $queryMyVersion . "<br>";
+list ($mayor, $minor) = convierteNumeroVersion($lastVersion);
+if($myversion < $lastVersion )
+{ //Complete de current version with new platform.
+}
+else
+{ //Create a new version number.
       $minor+=1;
       if($minor>99){
 	      $minor=0;
 	      $mayor+=1;
       }
+}
       $nextVersion=$mayor.".".$minor;
 echo "mayor : " . $mayor . " minor:" . $minor . " nextVersion : " . $nextVersion . "<br>";
       $salida=shell_exec('../../bin/creaNuevaVersion.sh '. $nextVersion . ' ' . $_REQUEST["platform"] );
