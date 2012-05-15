42d9c86cf0fe3395075888cfabaab43540ce36e7
rampxxxx <rampxxxx@gmail.com>
2012-05-15 19:51:35 +0200

    fix #24 . Se listan los modelos ya existentes.

--- html/user/emQueryModelo.php
+++ html/user/emQueryModelo.php
@@ -2,6 +2,7 @@
 require_once("../inc/db.inc");
 require_once("../inc/util.inc");
 require_once("../inc/countries.inc");
+require_once("../inc/utilidades.inc");
 
 
 db_init();
@@ -20,6 +21,20 @@ echo "
 echo "<form method=get action=emLanzaFormModelo.php>";
 echo form_tokens($user->authenticator);
 start_table();
+row1("List of Actual Models ", '9');
+echo "<tr><td>Model ID</td>";
+echo "<td width=\"15\">" . "Model Name     " . "</td>\n";
+echo "</tr>";
+row1("", '9');
+
+$models = get_mysql_model("SELECT modelo_id, nombre FROM modelo order by modelo_id ASC ");
+foreach($models as $model) {
+	$model_id = $model["modelo_id"];
+	$model_name = $model["nombre"];
+		echo "<tr><td>" . $model_id . "</td>";
+		echo "<td>" . $model_name . "</td>";
+		echo " </tr>";
+}
 row2(tra("MODEL NUMBER %1 to Create OR Delete %2", "<br><span class=note>", "</span>"),
     "<input name=modelNumber type=text size=10 >"
 );
