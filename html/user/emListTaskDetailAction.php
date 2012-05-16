c3a8be55cd5bede5a646c140a1f0c3b009322985
rampxxxx <rampxxxx@gmail.com>
2012-05-16 21:28:36 +0200

    #21 . Estructura para parsear datos y formatear.

--- html/user/emListTaskDetailAction.php
+++ html/user/emListTaskDetailAction.php
@@ -5,6 +5,52 @@
 require_once("../inc/db.inc");
 require_once("../inc/util.inc");
 require_once("../inc/countries.inc");
+function seccion($tok,$seccion)
+{
+$nombreSeccion="";
+if(stristr($tok,'#') != FALSE){
+	if(stristr($tok, '#MODEL') != FALSE) 
+	{
+		$nombreSeccion="#MODEL";
+	}
+	else if(stristr($tok, '#PARAMETERS') != FALSE)
+	{
+		$nombreSeccion="#PARAMETERS";
+	}
+	else if(stristr($tok, '#STEP') != FALSE)
+	{
+		$nombreSeccion="#STEP";
+	}
+	else if(stristr($tok, '#STIMULUS') != FALSE)
+	{
+		$nombreSeccion="#STIMULUS";
+	}
+	else if(stristr($tok, '#POST') != FALSE)
+	{
+		$nombreSeccion="#POST";
+	}
+	else if(stristr($tok, '#FILE_PARAMETERS') != FALSE)
+	{
+		$nombreSeccion="#FILE_PARAMETERS";
+	}
+	else if(stristr($tok, '#FILE_CURRENTS') != FALSE)
+	{
+		$nombreSeccion="#FILE_CURRENTS";
+	}
+	else if(stristr($tok, '#END') != FALSE)
+	{
+		$nombreSeccion="#END";
+	}
+}
+	if($nombreSeccion==$seccion || $nombreSeccion =="")
+	{
+		return $seccion;
+	}
+	else
+	{
+		return $nombreSeccion;
+	}
+}
 
 $size = (int) count($_REQUEST);
 
@@ -35,6 +81,18 @@ if($debug==1)
 	while($row=mysql_fetch_array($result))
 	{
 		echo "<tr><td> " . nl2br($row['parameters']) . "</td>";
+		$tok = strtok($row['parameters'], " \n\t");
+		while ($tok !== false) {
+			echo "<tr><td>$tok HAHA</td></tr>";
+			if(($seccion=seccion($tok,$seccion))=="#MODEL")
+			{
+				$result=mysql_query("select nombre from modelo where modelo_id=".);
+				$row=mysql_fetch_array($result);
+				row1($modelo,'9');
+			}
+			else if(($seccion=seccion($tok,$seccion))=="#PARAMETERS")
+				$tok = strtok(" \n\t");
+		}
 	}
 }
 else
