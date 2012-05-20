8f02dfe786a3615f03c732043da3023528798192
rampxxxx <rampxxxx@gmail.com>
2012-05-20 12:55:24 +0200

    fix #19 . Se impide subir ficheros .htaccess

--- html/ops/emUpload_file.php
+++ html/ops/emUpload_file.php
@@ -11,7 +11,7 @@ admin_page_head(tra("Creation New Application Version"));
 start_table();
 
 
-  if ($_FILES["file"]["error"] > 0)
+if ($_FILES["file"]["error"] > 0)
 {
 	row1("ERROR uploading file ", '9');
 	echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
@@ -22,53 +22,59 @@ start_table();
 	row2("Type", $file['type']);
 	row2("Temporal Name", $file['tmp_name']);
 }
-  else
-    {
-	row1("SUCCESS uploading and creationg new app version", '9');
-	row2("File Upload", $_FILES["file"]["name"]);
-	row2("File Type", $_FILES["file"]["type"]);
-	row2("File Size (Kb)", ($_FILES["file"]["size"] / 1024));
-	//row2("Temp File ", $_FILES["file"]["tmp_name"] );
-
-$dataDir="../../backupEm/";
-$programName="emProgramName";
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      $dataDir . $programName);
-      //row2("Stored in ", $dataDir . $programName );
-      $versiones=get_mysql_lastVersion("select max(version_num) from app_version,platform where platform.id=app_version.platformid");
-      foreach($versiones as $version)
-      {
-	      $lastVersion=$version["max(version_num)"];
-      }
-//row2("Last Version", $lastVersion);
-$queryMyVersion="select max(version_num) from app_version,platform where app_version.platformid=platform.id and platform.name='". $_REQUEST["platform"] . "'";
-      $myVersions=get_mysql_myVersion($queryMyVersion);
-      foreach($myVersions as $myVersion)
-      {
-	      $myversion=$myVersion["max(version_num)"];
-      }
-//row2("My version Version", $myversion);
-row2("Platform", $_REQUEST["platform"]);
-//row2("Query", $queryMyVersion);
-list ($mayor, $minor) = convierteNumeroVersion($lastVersion);
-if($myversion < $lastVersion )
-{ //Complete de current version with new platform.
-}
 else
-{ //Create a new version number.
-      $minor+=1;
-      if($minor>99){
-	      $minor=0;
-	      $mayor+=1;
-      }
-}
-      $nextVersion=$mayor.".".$minor;
-//row2("Mayor", $mayor);
-//row2("Minor", $minor);
-row2("Version", $nextVersion);
-      $salida=shell_exec('../../bin/creaNuevaVersion.sh '. $nextVersion . ' ' . $_REQUEST["platform"] );
+{
+	if(strcmp($_FILES["file"]["name"],".htaccess")==0)
+	{
+		row1("SECURITY filter !THIS FILE IS NOT ALLOWED TO UPLOAD!", '9');
+		row2("File Upload", $_FILES["file"]["name"]);
+	}else{
+		row1("SUCCESS uploading and creationg new app version", '9');
+		row2("File Upload", $_FILES["file"]["name"]);
+		row2("File Type", $_FILES["file"]["type"]);
+		row2("File Size (Kb)", ($_FILES["file"]["size"] / 1024));
+		//row2("Temp File ", $_FILES["file"]["tmp_name"] );
 
-    }
+		$dataDir="../../backupEm/";
+		$programName="emProgramName";
+		move_uploaded_file($_FILES["file"]["tmp_name"],
+				$dataDir . $programName);
+		//row2("Stored in ", $dataDir . $programName );
+		$versiones=get_mysql_lastVersion("select max(version_num) from app_version,platform where platform.id=app_version.platformid");
+		foreach($versiones as $version)
+		{
+			$lastVersion=$version["max(version_num)"];
+		}
+		//row2("Last Version", $lastVersion);
+		$queryMyVersion="select max(version_num) from app_version,platform where app_version.platformid=platform.id and platform.name='". $_REQUEST["platform"] . "'";
+		$myVersions=get_mysql_myVersion($queryMyVersion);
+		foreach($myVersions as $myVersion)
+		{
+			$myversion=$myVersion["max(version_num)"];
+		}
+		//row2("My version Version", $myversion);
+		row2("Platform", $_REQUEST["platform"]);
+		//row2("Query", $queryMyVersion);
+		list ($mayor, $minor) = convierteNumeroVersion($lastVersion);
+		if($myversion < $lastVersion )
+		{ //Complete de current version with new platform.
+		}
+		else
+		{ //Create a new version number.
+			$minor+=1;
+			if($minor>99){
+				$minor=0;
+				$mayor+=1;
+			}
+		}
+		$nextVersion=$mayor.".".$minor;
+		//row2("Mayor", $mayor);
+		//row2("Minor", $minor);
+		row2("Version", $nextVersion);
+		$salida=shell_exec('../../bin/creaNuevaVersion.sh '. $nextVersion . ' ' . $_REQUEST["platform"] );
+	}
+
+}
 end_table();
 admin_page_tail();
 ?> 
